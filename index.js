c7ae68f88b13f5b5c09e3c84dab840f91710ffd5
Alexa <kusteralexa@gmail.com>
2022-11-03 23:00:34 -0400

    commit tree fdb703146d2388e84034f2d288a624d4fc38142a

---
+++ index.js
@@ -0,0 +1,57 @@
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add(1 + 2));
+
+
+function subtract(a, b) {
+    return a - b;
+}
+console.log(subtract(7 - 6));
+
+
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(7 * 6));
+
+function divide(a, b) {
+    return a / b;
+}
+console.log(divide(24 / 6));
+
+
+function increment(number) {
+    return ++number;
+}
+
+function decrement(number) {
+    return --number
+}
+
+function makeInt(str) {
+    const result = parseInt(str, 10);
+    return result;
+}
+
+function preserveDecimal (str) {
+    const result = parseFloat(str, 10);
+    return result;
+}
+
